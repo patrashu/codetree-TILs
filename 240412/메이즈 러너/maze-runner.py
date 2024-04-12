884b027732b15de129bf3e945d77b853a13bb11b
Young-Il Kim <78347296+patrashu@users.noreply.github.com>
2024-04-12 12:06:12 +0900

    Title: 메이즈 러너, Time: 184ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240412/메이즈 러너/maze-runner.py
+++ 240412/메이즈 러너/maze-runner.py
@@ -1,191 +1,148 @@
-n, m, k = tuple(map(int, input().split()))
-# 모든 벽들의 상태를 기록해줍니다.
-board = [
-    [0] * (n + 1)
-    for _ in range(n + 1)
-]
+"""
+1. N * N 미로
+2. 각 칸은 3가지 상태 중 하나를 가진다
+- 빈칸 : 이동 가능한 칸
+- 벽 : 이동할 수 없는 칸 / 내구도가 1~9 / 회전하면 1씩 깎임 / 0되면 빈칸
+- 출구 : 도착하면 즉시 탈출
 
-for i in range(1, n + 1):
-    board[i] = [0] + list(map(int, input().split()))
+3. 1초마다 한 칸씩 움직임
+- 동시에 움직인다
+- 상하좌우로 움직일 수 있으며, 벽이 없는 곳으로만 이동 가능
+- 움직이는 칸이 현재 머무르던 칸보다 맨해튼 거리가 가까워야함
+- 2개 이상이면 상/하 우선
+- 몬움직이면 x
+- 한 칸에 여러명이 있을 수 있음
 
-# 회전의 구현을 편하게 하기 위해 2차원 배열을 하나 더 정의해줍니다.
-next_board = [
-    [0] * (n + 1)
-    for _ in range(n + 1)
-]
+4. 이동했으면 회전함
+- 한 명 이상의 참가자 + 출구를 포함하는 가장 작은 정사각형 searching
+- 여러 개면 r이 작은 것을 우선, 그래도 같은면 c 우선
+- 선택 영역은 왼쪽 90도 회전, 내구도 -= 1
 
-# 참가자의 위치 정보를 기록해줍니다.
-traveler = [(-1, -1)] + [
-    tuple(map(int, input().split()))
-    for _ in range(m)
-]
+5. K초 동안 반복, 그 전에 끝나면 종료
+- 종료 후 모든 참가자 이동 거리 합 + 출구 좌표 출력
+"""
+from copy import deepcopy
+from collections import defaultdict
 
-# 출구의 위치 정보를 기록해줍니다.
-exits = tuple(map(int, input().split()))
+def rotate(matrix):
+    return [list(mtrx) for mtrx in zip(*matrix[::-1])]
 
-# 정답(모든 참가자들의 이동 거리 합)을 기록해줍니다.
-ans = 0
+if __name__ == '__main__':
+    N, M, K = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    direc = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+    player_to_pid = defaultdict(list)
+    pid_to_player = {}
 
-# 회전해야 하는 최소 정사각형을 찾아 기록해줍니다.
-sx, sy, square_size = 0, 0, 0
+    for i in range(M):
+        a, b = map(int, input().split())
+        player_to_pid[(a-1, b-1)].append(i)
+        pid_to_player[i] = [a-1, b-1]
 
+    px, py = map(int, input().split())
+    px, py = px-1, py-1
 
-# 모든 참가자를 이동시킵니다.
-def move_all_traveler():
-    global exits, ans
-
-    # m명의 모든 참가자들에 대해 이동을 진행합니다.
-    for i in range(1, m + 1):
-        # 이미 출구에 있는 경우 스킵합니다.
-        if traveler[i] == exits:
-            continue
-        
-        tx, ty = traveler[i]
-        ex, ey = exits
-
-        # 행이 다른 경우 행을 이동시켜봅니다.
-        if tx != ex:
-            nx, ny = tx, ty
-
-            if ex > nx: 
-                nx += 1
-            else:
-                nx -= 1
-
-            # 벽이 없다면 행을 이동시킬 수 있습니다.
-            # 이 경우 행을 이동시키고 바로 다음 참가자로 넘어갑니다.
-            if not board[nx][ny]:
-                traveler[i] = (nx, ny)
-                ans += 1
-                continue
-
-        # 열이 다른 경우 열을 이동시켜봅니다.
-        if ty != ey:
-            nx, ny = tx, ty
-
-            if ey > ny: 
-                ny += 1
-            else:
-                ny -= 1
-
-            # 벽이 없다면 행을 이동시킬 수 있습니다.
-            # 이 경우 열을 이동시킵니다.
-            if not board[nx][ny]:
-                traveler[i] = (nx, ny)
-                ans += 1
-                continue
-
-
-# 한 명 이상의 참가자와 출구를 포함한 가장 작은 정사각형을 찾습니다.
-def find_minimum_square():
-    global exits, sx, sy, square_size
-    ex, ey = exits
-
-    # 가장 작은 정사각형부터 모든 정사각형을 만들어봅니다.
-    for sz in range(2, n + 1):
-        # 가장 좌상단 r 좌표가 작은 것부터 하나씩 만들어봅니다.
-        for x1 in range(1, n - sz + 2):
-            # 가장 좌상단 c 좌표가 작은 것부터 하나씩 만들어봅니다.
-            for y1 in range(1, n - sz + 2):
-                x2, y2 = x1 + sz - 1, y1 + sz - 1
-
-                # 만약 출구가 해당 정사각형 안에 없다면 스킵합니다.
-                if not (x1 <= ex and ex <= x2 and y1 <= ey and ey <= y2):
+    cnt, time, move = M, K, 0
+    while cnt and time:
+        # move
+        exit_pid = []
+        for pid, (cx, cy) in pid_to_player.items():
+            candits = []
+            bound = abs(px-cx) + abs(py-cy)
+            for k in range(4):
+                nx, ny = cx+direc[k][0], cy+direc[k][1]
+                if nx < 0 or nx >= N or ny < 0 or ny >= N or arr[nx][ny] != 0:
                     continue
+                tmp = abs(px-nx) + abs(py-ny)
+                if tmp < bound:
+                    candits.append((k, tmp, nx, ny))
+            
+            # check
+            if not candits:
+                continue
+            
+            # update
+            candits.sort(key=lambda x: (x[1], x[0]))
+            _, _, nx, ny = candits[0]
+            print(pid, cx, cy, nx, ny)
+            if (px, py) == (nx, ny):
+                exit_pid.append(pid)
 
-                # 한 명 이상의 참가자가 해당 정사각형 안에 있는지 판단합니다.
-                is_traveler_in = False
-                for l in range(1, m + 1):
-                    tx, ty = traveler[l]
-                    if x1 <= tx and tx <= x2 and y1 <= ty and ty <= y2:
-                        # 출구에 있는 참가자는 제외합니다.
-                        if not (tx == ex and ty == ey):
-                            is_traveler_in = True
+            pid_to_player[pid] = [nx, ny]
+            _idx = player_to_pid[(cx, cy)].index(pid)
+            player_to_pid[(cx, cy)].pop(_idx)
+            player_to_pid[(nx, ny)].append(pid)
+            move += 1
 
-                # 만약 한 명 이상의 참가자가 해당 정사각형 안에 있다면
-                # sx, sy, square_size 정보를 갱신하고 종료합니다.
-                if is_traveler_in:
-                    sx = x1
-                    sy = y1
-                    square_size = sz
+        # remove
+        for pid in exit_pid:
+            tx, ty = pid_to_player[pid]
+            del pid_to_player[pid]
+            _idx = player_to_pid[(tx, ty)].index(pid)
+            player_to_pid[(tx, ty)].pop(_idx)
+            cnt -= 1
+        
+        if cnt == 0:
+            break
 
-                    return
+        # find sub arr
+        ans = []
+        grid = 2
+        while grid <= N:
+            for p in range(0, N-grid+1):
+                for q in range(0, N-grid+1):
+                    exit_pos, player_pos = [], []
+                    for i in range(p, p+grid):
+                        for j in range(q, q+grid):
+                            if (px, py) == (i, j): # if
+                                exit_pos.append((i, j))
+                            elif len(player_to_pid[(i, j)]) > 0:
+                                player_pos.extend(player_to_pid[(i, j)])
 
+                    if len(exit_pos) > 0 and len(player_pos) > 0:
+                        ans.append((p, q, grid, exit_pos, player_pos))
 
-# 정사각형 내부의 벽을 회전시킵니다.
-def rotate_square():
-    # 우선 정사각형 안에 있는 벽들을 1 감소시킵니다.
-    for x in range(sx, sx + square_size):
-        for y in range(sy, sy + square_size):
-            if board[x][y]: 
-                board[x][y] -= 1
+            if len(ans) > 0:
+                break
+            else:
+                grid += 1
 
-    # 정사각형을 시계방향으로 90' 회전합니다.
-    for x in range(sx, sx + square_size):
-        for y in range(sy, sy + square_size):
-            # Step 1. (sx, sy)를 (0, 0)으로 옮겨주는 변환을 진행합니다. 
-            ox, oy = x - sx, y - sy
-            # Step 2. 변환된 상태에서는 회전 이후의 좌표가 (x, y) . (y, square_n - x - 1)가 됩니다.
-            rx, ry = oy, square_size - ox - 1
-            # Step 3. 다시 (sx, sy)를 더해줍니다.
-            next_board[rx + sx][ry + sy] = board[x][y]
+        ans.sort(key=lambda x: (x[2], x[0], x[1]))
+        # rotate and update
+        p, q, grid, exit_pos, player_pos = ans[0]
+        print(p, q, grid, exit_pos, player_pos)
+        for tx, ty in exit_pos:
+            arr[tx][ty] = -11
 
-    # next_board 값을 현재 board에 옮겨줍니다.
-    for x in range(sx, sx + square_size):
-        for y in range(sy, sy + square_size):
-            board[x][y] = next_board[x][y]
+        bef_to_cur = {}
+        for i in range(p, p+grid):
+            for j in range(q, q+grid):
+                ox, oy = i-p, j-q
+                rx, ry = oy, grid-ox-1
+                bef_to_cur[(rx+p, ry+q)] = [i, j]
+        new_arr = deepcopy(arr)
 
+        for (tx, ty), (cx, cy) in bef_to_cur.items():
+            new_arr[tx][ty] = arr[cx][cy]
+            if new_arr[tx][ty] == -11:
+                new_arr[tx][ty] = 0
+                px, py = tx, ty
+            elif new_arr[tx][ty] > 0:
+                new_arr[tx][ty] -= 1
+        
+        visit = [False]*M
+        for (tx, ty), (cx, cy) in bef_to_cur.items():
+            for _pid in player_pos:
+                ccx, ccy = pid_to_player[_pid]
+                if not visit[_pid] and (ccx, ccy) == (cx, cy):
+                    _idx = player_to_pid[(cx, cy)].index(_pid)
+                    player_to_pid[(cx, cy)].pop(_idx)
+                    player_to_pid[(tx, ty)].append(_pid)
+                    pid_to_player[_pid] = [tx, ty]
+                    visit[_pid] = True
 
-# 모든 참가자들 및 출구를 회전시킵니다.
-def rotate_traveler_and_exit():
-    global exits
-
-    # m명의 참가자들을 모두 확인합니다.
-    for i in range(1, m + 1):
-        tx, ty = traveler[i]
-        # 해당 참가자가 정사각형 안에 포함되어 있을 때에만 회전시킵니다.
-        if sx <= tx and tx < sx + square_size and sy <= ty and ty < sy + square_size:
-            # Step 1. (sx, sy)를 (0, 0)으로 옮겨주는 변환을 진행합니다. 
-            ox, oy = tx - sx, ty - sy
-            # Step 2. 변환된 상태에서는 회전 이후의 좌표가 (x, y) . (y, square_n - x - 1)가 됩니다.
-            rx, ry = oy, square_size - ox - 1
-            # Step 3. 다시 (sx, sy)를 더해줍니다.
-            traveler[i] = (rx + sx, ry + sy)
-
-    # 출구에도 회전을 진행합니다.
-    ex, ey = exits
-    if sx <= ex and ex < sx + square_size and sy <= ey and ey < sy + square_size:
-        # Step 1. (sx, sy)를 (0, 0)으로 옮겨주는 변환을 진행합니다. 
-        ox, oy = ex - sx, ey - sy
-        # Step 2. 변환된 상태에서는 회전 이후의 좌표가 (x, y) . (y, square_n - x - 1)가 됩니다.
-        rx, ry = oy, square_size - ox - 1
-        # Step 3. 다시 (sx, sy)를 더해줍니다.
-        exits = (rx + sx, ry + sy)
-
-
-for _ in range(k):
-    # 모든 참가자를 이동시킵니다.
-    move_all_traveler()
-
-    # 모든 사람이 출구로 탈출했는지 판단합니다.
-    is_all_escaped = True
-    for i in range(1, m + 1):
-        if traveler[i] != exits:
-            is_all_escaped = False
-
-    # 만약 모든 사람이 출구로 탈출했으면 바로 종료합니다.
-    if is_all_escaped: 
-        break
-
-    # 한 명 이상의 참가자와 출구를 포함한 가장 작은 정사각형을 찾습니다.
-    find_minimum_square()
-
-    # 정사각형 내부의 벽을 회전시킵니다.
-    rotate_square()
-    # 모든 참가자들 및 출구를 회전시킵니다.
-    rotate_traveler_and_exit()
-
-print(ans)
-
-ex, ey = exits
-print(ex, ey)
+        arr = new_arr
+        time -= 1
+   
+    print(move)
+    print(px+1, py+1)
