3c3ca52a46d7250dce97730bd0688a2843dbe26f
Young-Il Kim <78347296+patrashu@users.noreply.github.com>
2024-07-10 09:56:24 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 485ms, Memory: 32MB, Status: Wrong Answer - CodeTree

--- 240710/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240710/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -3,9 +3,20 @@ from collections import deque
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 dp = [[1]*n for _ in range(n)]
+direction = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
 
 for i in range(n):
     for j in range(n):
+        flag = False
+        for dx, dy in direction:
+            nx, ny = i+dx, j+dy
+            if 0 <= nx < n and 0 <= ny < n and arr[i][j] < arr[nx][ny]:
+                flag = True
+                break
+
+        if not flag:
+            continue
 
         # bfs
         dq = deque([(i, j)])
@@ -14,14 +25,14 @@ for i in range(n):
 
         while dq:
             cx, cy = dq.popleft()
-            for dx, dy in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+            for dx, dy in direction:
                 nx, ny = cx+dx, cy+dy
                 if nx < 0 or nx >= n or ny < 0 or ny >= n or (nx, ny) in visit:
                     continue
 
                 # 값이 작으면서 경로를 갱신할 수 있을 때
-                if (arr[i][j] < arr[nx][ny]) and (dp[nx][ny] < dp[i][j] + 1):
-                    dp[nx][ny] = dp[i][j]+1
+                if (arr[cx][cy] < arr[nx][ny]) and (dp[nx][ny] < dp[cx][cy] + 1):
+                    dp[nx][ny] = dp[cx][cy]+1
                     visit.add((nx, ny))
                     dq.append((nx, ny))
 
